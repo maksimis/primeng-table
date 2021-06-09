56653aa121703bc3bd60ab5517a36b07f418f1e6
Max Isankin <misankin@techinline.com>
2021-06-09 16:31:21 +0300

    primeng 8.2.9 bug reproducing stored table + filter + sort + resizableColumns

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Customer } from './customer';
+import { CustomerService } from "./customerservice";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'primeng-table-test';
+export class AppComponent implements OnInit {
+
+  customers1: Customer[] = [];
+
+  selectedCustomer1: Customer = {};
+
+  constructor(private customerService: CustomerService) { }
+
+  ngOnInit() {
+    this.customerService.getCustomersMedium().then(data => this.customers1 = data);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TableModule } from "primeng-lts/table";
+import { CustomerService } from "./customerservice";
+import { HttpClientModule } from "@angular/common/http";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    TableModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    CustomerService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
